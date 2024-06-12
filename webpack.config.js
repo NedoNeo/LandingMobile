cdb834a4b57424e5a3e1ff5fde50496ae5bdaa53
NedoNeo <denshkur@yandex.ru>
2024-06-12 18:04:09 +0300

    new Pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/js/script.js',
   output: {
-    path: path.resolve(__dirname, 'dist'), // каталог для результатов сборки 
-    filename: 'index.js', // имя файла с результатами сборки (должно совпадать с entry point в шаге 1)
-    //library: 'ajs', // название нашей библиотеки
-    libraryTarget: 'umd', // UMD (Universal Module Definition https://github.com/umdjs/umd) - шаблон, который позволяет использовать RequireJS и браузер
-    libraryExport: 'default', // экспортируется имя default
-    globalObject: 'this', // что принимать за глобальный объект, иначе сгенерируется window, а его, как вы знаете, на платформе Node.js нет
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
-      // {
-      //   test: /\.js$/,
-      //   exclude: /node_modules/,
-      //   use: {
-      //     loader: 'babel-loader',
-      //   },
-      // },
-
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
       {
         test: /\.css$/,
         use: [
@@ -30,21 +24,19 @@ module.exports = {
             options: {
               postcssOptions: {
                 plugins: [
-                  require('autoprefixer')
-                ]
-              }
-            }
-          }
-        ]
+                  require('autoprefixer'),
+                ],
+              },
+            },
+          },
+        ],
       },
     ],
   },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
-    },
-    compress: true,
-    port: 8000,
-  },
-  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  mode: 'development', // Или 'production' в зависимости от ваших нужд
 };
